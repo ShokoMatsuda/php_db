82058b67395970e14d875a93f9244324e8c71d7c
ShokoMatsuda <syokom.0128@gmail.com>
2023-12-14 21:25:04 +0900

    1.7 try-catch文を使って例外処理を実行してみよう

--- connect.php
+++ connect.php
@@ -11,8 +11,16 @@
         $user = 'root';
         $password = 'root';
 
-        $pdo = new PDO($dsn, $user, $password);
-        echo 'データベースの接続に成功しました。';
+        try {
+            // データベースの接続を試行する
+            $pdo = new PDO($dsn, $user, $password);
+
+            // データベースの接続に成功した場合の処理
+            echo 'データベースの接続に成功しました。';
+        } catch (PDOException $e) {
+            // データベースの接続に失敗した場合の処理
+            exit ('データベースの接続に失敗しました。' . $e->getMessage());
+        }
         ?>
     </p>
 </body>
