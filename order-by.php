5ea485a0b374a09cb4c6906eb4b2f7917585ba3c
ShokoMatsuda <syokom.0128@gmail.com>
2023-12-17 23:46:41 +0900

    9.4 ユーザー一覧を作成しよう

---
+++ order-by.php
@@ -0,0 +1,52 @@
+<?php
+        $dsn = 'mysql:dbname=php_db;host=localhost;charset=utf8mb4';
+        $user = 'root';
+        $password = 'root';
+
+        try {
+            $pdo = new PDO($dsn, $user, $password);
+
+            // usersテーブルからid、name、ageカラムのデータを取得するためのSQL文を変数$sqlに代入する
+            $sql = 'SELECT id, name, age FROM users';
+
+            // SQL文を実行する
+            $stmt = $pdo->query($sql);
+
+            // SQL文の実行結果を配列で取得する
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            exit($e->getMessage());
+        }
+        ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>PHP+DB</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>氏名</th>
+        <th>年齢</th>
+    </tr>
+    <?php
+    // 配列の中身を順番に取り出し、表形式で出力する
+    foreach ($results as $result) {
+        $table_row = "
+        <tr>
+        <td>{$result['id']}</td>
+        <td>{$result['name']}</td>
+        <td>{$result['age']}</td>
+        </tr>
+        ";
+        echo $table_row;
+    }
+    ?>
+</table>
+</body>
+</html>
