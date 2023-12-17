a155a9fe6ad5366971140d83982dc464c44c64e2
ShokoMatsuda <syokom.0128@gmail.com>
2023-12-17 23:21:26 +0900

    7.5 UPDATE文を使って更新機能を作成しよう

--- update.php
+++ update.php
@@ -3,17 +3,23 @@ $dsn = 'mysql:dbname=php_db;host=localhost;charset=utf8mb4';
 $user = 'root';
 $password = 'root';
 
-// submitパラメータの値が存在するとき（「登録」ボタンを押したとき）の処理
+// submitパラメータの値が存在するとき（「更新」ボタンを押したとき）の処理
 if (isset($_POST['submit'])) {
     try {
         $pdo = new PDO($dsn, $user, $password);
 
-        // 動的に変わる値をプレースホルダに置き換えたINSERT文をあらかじめ用意する
+        // 動的に変わる値をプレースホルダに置き換えたUPDATE文をあらかじめ用意する
         $sql = '
-                INSERT INTO users (name, furigana, email, age, address)
-                VALUES (:name, :furigana, :email, :age, :address)
+                UPDATE users
+                SET name = :name,
+                furigana = :furigana,
+                email = :email,
+                age = :age,
+                address = :address
+                WHERE id = :id
                 ';
-                $stmt = $pdo->prepare($sql);
+
+        $stmt = $pdo->prepare($sql);
 
         // bindValue()メソッドを使って実際の値をプレースホルダにバインドする（割り当てる）
         $stmt->bindValue(':name', $_POST['user_name'], PDO::PARAM_STR);
@@ -21,17 +27,19 @@ if (isset($_POST['submit'])) {
         $stmt->bindValue(':email', $_POST['user_email'], PDO::PARAM_STR);
         $stmt->bindValue(':age', $_POST['user_age'], PDO::PARAM_INT);
         $stmt->bindValue(':address', $_POST['user_address'], PDO::PARAM_STR);
+        $stmt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
 
         // SQL文を実行する
         $stmt->execute();
 
-        // header()関数を使ってselect.phpにリダイレクトさせる
-        header('Location: select.php');
+        // header()関数を使ってusers.phpにリダイレクトさせる
+        header('Location: users.php');
     } catch (PDOException $e) {
         exit($e->getMessage());
     }
 }
 
+// submitパラメータの値が存在するとき（「更新」ボタンを押したとき）の処理
 if (isset($_GET['id'])) {
     try {
         $pdo = new PDO($dsn, $user, $password);
@@ -52,7 +60,7 @@ if (isset($_GET['id'])) {
 
         // idパラメータの値と同じidのデータが存在しない場合はエラーメッセージを表示して処理を終了する
         // 補足：fetch()メソッドは実行結果が取得できなかった場合にFALSEを返す
-        if ($user == FALSE) {
+        if ($user === FALSE) {
             exit('idパラメータの値が不正です。');
         }
     } catch (PDOException $e) {
@@ -74,24 +82,24 @@ if (isset($_GET['id'])) {
 <body>
     <h1>ユーザー更新</h1>
     <p>更新する内容を入力してください。</p>
-    <form action="update.php" method="post">
+    <form action="update.php?id=<?= $_GET['id'] ?>" method="post">
     <div>
         <label for="user_name">お名前<span>【必須】</span></label>
-        <input type="text" name="user_name" value="<?= $user['name'] ?> "maxlength="60" required>
+        <input type="text" name="user_name" value="<?= $user['name'] ?>" maxlength="60" required>
 
         <label for="user_furigana">ふりがな<span>【必須】</span></label>
-        <input type="text" name="user_furigana" value="<?= $user['furigana'] ?> maxlength="60" required>
+        <input type="text" name="user_furigana" value="<?= $user['furigana'] ?>" maxlength="60" required>
 
         <label for="user_email">メールアドレス<span>【必須】</span></label>
-        <input type="email" name="user_email" value="<?= $user['email'] ?> maxlength="255" required>
+        <input type="email" name="user_email" value="<?= $user['email'] ?>" maxlength="255" required>
 
         <label for="user_age">年齢</label>
-        <input type="number" name="user_age" value="<?= $user['age'] ?> min="13" max="130">
+        <input type="number" name="user_age" value="<?= $user['age'] ?>" min="13" max="130">
 
         <label for="user_address">住所</label>
-        <input type="text" name="user_address" value="<?= $user['address'] ?> maxlength="255">
+        <input type="text" name="user_address" value="<?= $user['address'] ?>" maxlength="255">
     </div>
-    <button type="submit" name="submit" value="insert">更新</button>
+    <button type="submit" name="submit" value="update">更新</button>
     </form>
 </body>
 </html>
